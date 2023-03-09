1e03b7e01fc19802115c92a9e358f610e562f40e
Dmitry Perchanov <dmitry.perchanov@intel.com>
2023-03-09 16:31:51 +0200

    ipu6 udev rules for bind and rs-enum

--- scripts/d457_init.sh
+++ scripts/d457_init.sh
@@ -27,7 +27,9 @@ declare -A media_mux_csi2_link=( [a]=0 [b]=1 [c]=2 [d]=3 )
 #declare -A media_mux_csi2_link=( [c]=0 [b]=1 [a]=2 [d]=3 )
 
 
-declare -A serdes_mux_i2c_bus=( [a]=2 [b]=2 [c]=4 [d]=4 )
+#declare -A serdes_mux_i2c_bus=( [a]=2 [b]=2 [c]=4 [d]=4 )
+declare -A serdes_mux_i2c_bus=( [a]=5 [b]=5 [c]=5 [d]=5 )
+
 
 csi2_be_soc="CSI2 BE SOC ${media_mux_csi2_link[${mux}]}"
 
