2ee29b2b5a753fca2d58df8538419086e8ff72cf
Dmitry Perchanov <dmitry.perchanov@intel.com>
2023-03-19 15:32:55 +0200

    docker build artifactory path
    Signed-off-by: Dmitry Perchanov <dmitry.perchanov@intel.com>

--- build/build.sh
+++ build/build.sh
@@ -7,31 +7,33 @@
 
 # verbose script running
 set -x
-PWD=$(pwd)
+PWD_LC=$(pwd)
 # source of linux-headers-5.15.49-for-ubuntu_5.15.49-for-ubuntu-1_amd64.deb
-PKG_PATH=${PWD}/../artifactory/
-PKG=${PWD}/pkg
-RESULTS=${PWD}/results/
+PKG_PATH=${PWD_LC}/../artifactory/
+PKG=${PWD_LC}/pkg
+RESULTS=${PWD_LC}/results/
 IOTG_REPO="https://github.com/dmipx/ipu6-drivers.git"
 IOTG_BRANCH="realsense_ipu6"
 RS_IOTG_PATCH="https://github.com/IntelRealSense/d4xx_mipi_sensor_driver_adlp.git"
 
-mkdir ${PKG} && cp ${PKG_PATH}/* ${PKG}
-sudo dpkg -i ${PKG}/*.deb
-rm -rf ${PKG}
+#mkdir ${PKG} && cp ${PKG_PATH}/* ${PKG}
+#sudo dpkg -i ${PKG}/*.deb
+#rm -rf ${PKG}
 git clone --depth 1 --branch ${IOTG_BRANCH} --single-branch ${IOTG_REPO}
 
-cd ${PWD}/ipu6-drivers
+cd ${PWD_LC}/ipu6-drivers
 #update uapi header
-sudo cp include/uapi/linux/ipu-isys.h /usr/src/linux-headers-5.15.49-for-ubuntu/include/uapi/linux/ipu-isys.h
+cp include/uapi/linux/ipu-isys.h /usr/src/linux-headers-5.15.49-for-ubuntu/include/uapi/linux/ipu-isys.h
 
 #build modules
 make -j -C /usr/src/linux-headers-5.15.49-for-ubuntu/ M=$(pwd)
+chown -R --reference=${PWD_LC} $(pwd)
 
 mkdir -p ${RESULTS}
 
 # copy artifacts
-cp ${PWD}/ipu6-drivers/drivers/media/i2c/d4xx.ko ${RESULTS}
-cp ${PWD}/ipu6-drivers/drivers/media/i2c/max9295.ko ${RESULTS}
-cp ${PWD}/ipu6-drivers/drivers/media/i2c/max9296.ko ${RESULTS}
-cp ${PWD}/ipu6-drivers/drivers/media/pci/intel/ipu6/intel-ipu6-isys.ko ${RESULTS}
+cp ${PWD_LC}/ipu6-drivers/drivers/media/i2c/d4xx.ko ${RESULTS}
+cp ${PWD_LC}/ipu6-drivers/drivers/media/i2c/max9295.ko ${RESULTS}
+cp ${PWD_LC}/ipu6-drivers/drivers/media/i2c/max9296.ko ${RESULTS}
+cp ${PWD_LC}/ipu6-drivers/drivers/media/pci/intel/ipu6/intel-ipu6-isys.ko ${RESULTS}
+chown -R --reference=${PWD_LC} ${RESULTS}
---
+++ build/run.sh
@@ -0,0 +1,1 @@
+docker run --rm -it -v /home/dima/work/git/dmipx/ipu6-ds5-adlp/build:/build/ local/ipu6-env /bin/bash -c 'cd /build && /build/build.sh'
